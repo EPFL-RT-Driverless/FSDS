30c68825f837d9baddc7e17602846e599ee60127
MattBrth <36452374+MattBrth@users.noreply.github.com>
2022-12-17 22:37:57 +0100

    Set steering angle max (35°) & init script

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,18 +7,39 @@ file=~/Formula-Student-Driverless-Simulator/settings.json
 
 if [ -d $dir ];
 then
-    echo "Folder already exists"
+    echo "Folder $dir already exists"
 else
-    echo "Creating folder"
-    mkdir ~/Formula-Student-Driverless-Simulator
+    echo "Creating $dir "
+    mkdir $dir
 fi
 
 if [ -f $file ];
 then
-    echo "Settings file already exists"
+    _prompt="Settings file already exists, do you want to override it [y/n] ?"
+
+    # Loop forever until the user enters a valid response (Y/N or Yes/No).
+    while true; do
+        read -r -p "$_prompt " _response
+        case "$_response" in
+            [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
+            curl -O https://raw.githubusercontent.com/EPFL-RT-Driverless/FSDS/master/settings.json
+            echo "Creating settings files"
+            mv settings.json $dir
+            break
+            ;;
+            [Nn][Oo]|[Nn])  # No or N.
+            echo "Previous settings file kept, setup finished !"
+            exit 0
+            ;;
+            *) # Anything else (including a blank) is invalid.
+            ;;
+        esac
+    done
 else
     echo "Creating settings files"
-    cp ./settings.json ~/Formula-Student-Driverless-Simulator
+    curl -O https://raw.githubusercontent.com/EPFL-RT-Driverless/FSDS/master/settings.json
+    echo "Creating settings files"
+    mv settings.json $dir
 fi
 
 echo "Setup finished !"
